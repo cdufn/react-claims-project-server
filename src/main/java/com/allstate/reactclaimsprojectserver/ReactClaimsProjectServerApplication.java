2612e3945054462f48d74b7891378f026a8acb3f
Duff <cdufn@allstate.com>
2022-08-03 12:46:02 +0000

    first commit

---
+++ src/main/java/com/allstate/reactclaimsprojectserver/ReactClaimsProjectServerApplication.java
@@ -0,0 +1,13 @@
+package com.allstate.reactclaimsprojectserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactClaimsProjectServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactClaimsProjectServerApplication.class, args);
+	}
+
+}
